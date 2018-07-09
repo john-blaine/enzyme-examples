230dba18a74089ea1694b9ec9018057cb497edca
John Blaine <jablaine@uw.edu>
2018-07-08 21:09:36 -0600

    Add test for checking UserImage static html

--- src/UserImage.spec.js
+++ src/UserImage.spec.js
@@ -6,4 +6,9 @@ import { expect } from 'chai';
 // Component
 import UserImage from './UserImage';
 
-
+describe('<UserImage />', () => {
+  it('should have a <div /> element with .gh-widget-photo class', () => {
+    const wrapper = render(<UserImage />);
+    expect(wrapper.find('div').attr('class')).to.equal('gh-widget-photo');
+  });
+});
